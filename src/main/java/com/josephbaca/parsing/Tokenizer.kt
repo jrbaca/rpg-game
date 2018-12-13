41933cc5b5a5adbf345e28139b41629c0e3b3098
Joseph Baca <jrbaca@gmail.com>
2018-12-12 20:44:13 -0700

    Tokenizer works for one token

--- src/main/java/com/josephbaca/parsing/Tokenizer.kt
+++ src/main/java/com/josephbaca/parsing/Tokenizer.kt
@@ -1,5 +1,7 @@
 package com.josephbaca.parsing
 
+import java.lang.Integer.min
+
 /**
  *
  */
@@ -14,35 +16,50 @@ internal object Tokenizer {
         val preProcessedInput = preProcessInput(input)
 
         LOG.info("Attempting to tokenize \"%s\" with tokens %s".format(preProcessedInput, contextCommands))
-        return tokenizeUsingFullMatch(preProcessedInput, contextCommands)
+        return iterativelyMatchTokens(preProcessedInput, contextCommands)
     }
 
     private fun preProcessInput(input: String): String {
         return input.toLowerCase()
     }
 
-    private fun tokenizeUsingFullMatch(
+    private fun iterativelyMatchTokens(input: String, contextCommands: Set<ContextCommand>): List<ContextCommand>? {
+        return iterativelyMatchTokens(input, contextCommands, 0)
+    }
+
+    private fun iterativelyMatchTokens(
+        input: String,
+        contextCommands: Set<ContextCommand>,
+        index: Int
+    ): List<ContextCommand>? {
+
+        if (input.isEmpty()) return listOf()
+
+        val remainingTokens =
+            iterativelyMatchTokens(input.drop(1), contextCommands) ?: return null // Propagate bad matches
+
+        val matchingCommand =
+            getMatchingContextCommand(input, contextCommands) ?: return null // Bad if too many matches
+
+        return matchingCommand.plus(remainingTokens)
+        //TODO match on the correct substring
+    }
+
+    private fun getMatchingContextCommand(
         input: String,
         contextCommands: Set<ContextCommand>
     ): List<ContextCommand>? {
-        return try {
-            listOf(contextCommands.single { contextCommand ->
-                inputMatchesContextCommandRegex(input, contextCommand)
-            })
-        } catch (e: NoSuchElementException) {
-            LOG.warn("Nothing matches!")
-            null
-        } catch (e: IllegalArgumentException) {
+
+        val matchingCommands = contextCommands.filter { command -> inputMatchesContextCommandRegex(input, command) }
+        return if (matchingCommands.size > 1) {
             LOG.warn("Too many tokens match!")
             null
+        } else {
+            matchingCommands
         }
     }
 
-//    private fun iterativelyMatchTokens(input: String, contextCommands: Set<ContextCommand>): List<ContextCommand>? {
-//
-//    }
-
     private fun inputMatchesContextCommandRegex(input: String, contextCommand: ContextCommand): Boolean {
-        return input.matches(contextCommand.regex)
+        return input.trim().matches(contextCommand.regex)
     }
 }
--- src/test/java/com/josephbaca/parsing/TokenizerTest.kt
+++ src/test/java/com/josephbaca/parsing/TokenizerTest.kt
@@ -1,6 +1,5 @@
 package com.josephbaca.parsing
 
-import com.josephbaca.context.Battle
 import com.josephbaca.context.ContextManager
 import com.josephbaca.context.Room
 import org.junit.jupiter.api.Test
@@ -29,5 +28,40 @@ internal class TokenizerTest {
             val tokenList = Tokenizer.tokenizeInputWithContextCommandsRegex(input, allTokens)
             assertEquals(listOf(token), tokenList)
         }
+    }
+
+    @Test
+    fun tokenizeTwoTokens() {
+        val context = Room(contextManager)
+        val allTokens = context.contextCommands.keys
+
+        val token1 = allTokens.random()
+        val token2 = allTokens.random()
+
+        val input = "%s %s".format(token1.toString(), token2.toString())
+
+        val tokenList = Tokenizer.tokenizeInputWithContextCommandsRegex(input, allTokens)
+        assertEquals(listOf(token1, token2), tokenList)
+    }
+
+    @Test
+    fun tokenizeTwoTokensWithExtraWhitespace() {
+        val context = Room(contextManager)
+        val allTokens = context.contextCommands.keys
+
+        val token1 = allTokens.random()
+        val token2 = allTokens.random()
+
+        val input1 = "%s   %s   ".format(token1.toString(), token2.toString())
+        val tokenList1 = Tokenizer.tokenizeInputWithContextCommandsRegex(input1, allTokens)
+        assertEquals(listOf(token1, token2), tokenList1)
+
+        val input2 = "    %s   %s".format(token1.toString(), token2.toString())
+        val tokenList2 = Tokenizer.tokenizeInputWithContextCommandsRegex(input2, allTokens)
+        assertEquals(listOf(token1, token2), tokenList2)
+
+        val input3 = "   %s   %s   ".format(token1.toString(), token2.toString())
+        val tokenList3 = Tokenizer.tokenizeInputWithContextCommandsRegex(input3, allTokens)
+        assertEquals(listOf(token1, token2), tokenList3)
     }
 }
